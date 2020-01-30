93a82602b9a8c59f5b03800fc0cd10cb867c6390
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-30 22:39:58 +1100

    started

---
+++ src/com/codewardev/SudokuValidator.java
@@ -0,0 +1,12 @@
+package com.codewardev;
+
+// https://www.codewars.com/kata/529bf0e9bdf7657179000008/train/java
+
+public class SudokuValidator {
+
+	public static boolean check(int[][] sudoku) {
+		
+		return false;
+	}
+
+}

70384b12ff0a3a0c25491fe992ed9f2318ca95b4
Dale Wang <dalinwanglhtz@gmail.com>
2020-02-01 20:21:29 +1100

    best practice but complicated way for solving the problem - using factory method to replace multiple if-then-else statements

--- src/com/codewardev/SudokuValidator.java
+++ src/com/codewardev/SudokuValidator.java
@@ -3,6 +3,8 @@ package com.codewardev;
 //https://www.codewars.com/kata/529bf0e9bdf7657179000008/train/java
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SudokuValidator {
 
@@ -18,47 +20,20 @@ public class SudokuValidator {
 					return false;
 				}
 				hSquare[j][i] = sudoku[i][j];
-				if(i<3 && j<3) {
-					System.out.println("top left: "+sudoku[i][j]);
-					xSquare[0][counter[0]++] = sudoku[i][j];
-				} else
-				if(i<3 && j>=3 && j<6) {
-					System.out.println("top middle: "+sudoku[i][j]);
-					xSquare[1][counter[1]++] = sudoku[i][j];
-				} else
-				if(i<3 && j>=6) {
-					System.out.println("top right: "+sudoku[i][j]);
-					xSquare[2][counter[2]++] = sudoku[i][j];
-				} else
-				if(i>=3 && i<6 && j<3) {
-					System.out.println("middle left: "+sudoku[i][j]);
-					xSquare[3][counter[3]++] = sudoku[i][j];
-				} else
-				if(i>=3 && i<6 && j>=3 && j<6) {
-					System.out.println("middle middle: "+sudoku[i][j]);
-					xSquare[4][counter[4]++] = sudoku[i][j];
-				} else
-				if(i>=3 && i<6 && j>=6) {
-					System.out.println("middle right: "+sudoku[i][j]);
-					xSquare[5][counter[5]++] = sudoku[i][j];
-				} else
-				if(i>=6 && j<3) {
-					System.out.println("bottom left: "+sudoku[i][j]);
-					xSquare[6][counter[6]++] = sudoku[i][j];
-				} else
-				if(i>=6 && j>=3 && j<6) {
-					System.out.println("bottom middle: "+sudoku[i][j]);
-					xSquare[7][counter[7]++] = sudoku[i][j];
-				} else
-				if(i>=6 && j>=6) {
-					System.out.println("bottom right: "+sudoku[i][j]);
-					xSquare[8][counter[8]++] = sudoku[i][j];
-				}
+				
+				fillXSquare(i, j, xSquare, sudoku, counter);
 			}
 		}
 		
 		
 		return isAllDistinct(sudoku) && isAllDistinct(xSquare) && isAllDistinct(hSquare) ? true : false;
+	}
+
+	private static void fillXSquare(int i, int j, int[][] xSquare, int[][] sudoku, int[] counter) {
+		int count = OpsFactory.getOps(i, j);
+		if(count != -1) {
+			xSquare[count][counter[count]++] = sudoku[i][j];
+		}
 	}
 
 	private static boolean isAllDistinct(int[][] grid) {
@@ -68,14 +43,124 @@ public class SudokuValidator {
 		return true;
 	}
 
-	private static void printGrid(int[][] square) {
-		System.out.println("Square:");
-		for(int i=0; i<square.length; i++) {
-			for(int j=0; j<square[i].length; j++) {
-				System.out.print(square[i][j]+" ");
-			}
-			System.out.println();
-		}
-	}
+}
 
+
+class OpsFactory{
+	static Map<Integer, XSquareOps> opsMap = new HashMap<>();
+	static {
+		opsMap.put(0, new Zero());
+		opsMap.put(1, new One());
+		opsMap.put(2, new Two());
+		opsMap.put(3, new Three());
+		opsMap.put(4, new Four());
+		opsMap.put(5, new Five());
+		opsMap.put(6, new Six());
+		opsMap.put(7, new Seven());
+		opsMap.put(8, new Eight());
+	}
+	
+	public static int getOps(int i, int j){
+		for(Map.Entry<Integer, XSquareOps> a: opsMap.entrySet()) {
+			XSquareOps op = a.getValue();
+			if(op.isMet(i, j)) {
+				return a.getKey();
+			}
+		}
+		return -1;
+	}
+}
+
+interface XSquareOps{
+	boolean isMet(int i, int j);
+}
+
+class Zero implements XSquareOps{
+
+	@Override
+	public boolean isMet(int i, int j) {
+		if(i<3 && j<3) return true;
+		return false;
+	}
+	
+}
+
+class One implements XSquareOps{
+
+	@Override
+	public boolean isMet(int i, int j) {
+		if(i<3 && j>=3 && j<6) return true;
+		return false;
+	}
+	
+}
+
+class Two implements XSquareOps{
+
+	@Override
+	public boolean isMet(int i, int j) {
+		if(i<3 && j>=6) return true;
+		return false;
+	}
+	
+}
+
+class Three implements XSquareOps{
+
+	@Override
+	public boolean isMet(int i, int j) {
+		if(i>=3 && i<6 && j<3) return true;
+		return false;
+	}
+	
+}
+
+class Four implements XSquareOps{
+
+	@Override
+	public boolean isMet(int i, int j) {
+		if(i>=3 && i<6 && j>=3 && j<6) return true;
+		return false;
+	}
+	
+}
+
+class Five implements XSquareOps{
+
+	@Override
+	public boolean isMet(int i, int j) {
+		if(i>=3 && i<6 && j>=6) return true;
+		return false;
+	}
+	
+}
+
+class Six implements XSquareOps{
+
+	@Override
+	public boolean isMet(int i, int j) {
+		if(i>=6 && j<3) return true;
+		return false;
+	}
+	
+}
+
+class Seven implements XSquareOps{
+
+	@Override
+	public boolean isMet(int i, int j) {
+		if(i>=6 && j>=3 && j<6) return true;
+		return false;
+	}
+	
+}
+
+class Eight implements XSquareOps{
+
+	@Override
+	public boolean isMet(int i, int j) {
+		if(i>=6 && j>=6) return true;
+		return false;
+	}
+	
 }
